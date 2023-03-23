710dbe8d293ffa953f0bbca28fe7dbba90ceac06
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-23 09:42:33 -0700

    Update Mastermind.c

--- Mastermind.c
+++ Mastermind.c
@@ -5,48 +5,43 @@
 
 Int MasterMind()
 {
-	PatternGenerator();
+   PatternGenerator();
    char guess1, guess2, guess3, guess4;
-   int count =0;
+   int count = 0;
  
   printf("Type your 4 guesses: \n");
   scanf("%c" "%c" "%c" "%c" , &guess1, &guess2, &guess3, &guess4);
-	
+     
+  while ((guess1 != r1) && (guess2 != r2) && (guess3 != r3) && (guess4 != r4)) {
 // create if loop, if the answer is correct, print 1, if incorrect, print 0
-  if (guess == r1)
-	{
-		printf(“1 ”);
-  }
-  if (guess != r1)
-  {
-	  printf(“0 ”);
-  }
+    if (guess == r1){
+	printf(“1 ”);
+    }
+    if (guess != r1){
+	printf(“0 ”);
+    }
+    if (guess == r2){
+	printf(“1 ”);
+    }
+    if (guess != r2){
+	printf(“0 ”);
+    }
+    if (guess == r3){
+	printf(“1 ”);
+    }
+    if (guess != r3){
+	printf(“0 ”);
+    }
+    if (guess == r4){
+	printf(“1 ”);
+    }
+    if (guess != r4){
+	printf(“0 \n”);
+    }
+  count = count + 1
   
-  if (guess == r2)
-	{
-	  printf(“1 ”);
-  }
-  if (guess != r2)
-  {
-	  printf(“0 ”);
-  }
-  
-  if (guess == r3)
-	{
-		printf(“1 ”);
-  }
-  if (guess != r3)
-  {
-	  printf(“0 ”);
-  }
-  
-  if (guess == r4)
-	{
-		printf(“1 ”);
-  }
-  if (guess != r4)
-  {
-	  printf(“0 \n”);
+  printf("Type your 4 guesses: \n");
+  scanf("%c" "%c" "%c" "%c" , &guess1, &guess2, &guess3, &guess4);
   }
 
-{
+}
