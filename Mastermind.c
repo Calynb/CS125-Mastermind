ca39d657bc3bba1414b88cdc206e833ca1134d0b
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-23 09:31:28 -0700

    Update Mastermind.c

--- Mastermind.c
+++ Mastermind.c
@@ -6,9 +6,14 @@
 Int MasterMind()
 {
 	PatternGenerator();
-	int guess;
+   char guess1, guess2, guess3, guess4;
+   int count =0;
+ 
+  printf("Type your 4 guesses: \n");
+  scanf("%c" "%c" "%c" "%c" , &guess1, &guess2, &guess3, &guess4);
+	
 // create if loop, if the answer is correct, print 1, if incorrect, print 0
-	if (guess == r1)
+  if (guess == r1)
 	{
 		printf(“1 ”);
   }
