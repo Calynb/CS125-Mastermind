7c3865c9b60830ce82b86af6e9b046ef6a138f76
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-23 10:16:35 -0700

    Create PatternGen.h

---
+++ PatternGen.h
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+  typedef struct random_numbers
+  {
+    char num_1;
+    char num_2;
+    char num_3;
+    char num_4;
+  } rand_nums;
+  rand_nums;
