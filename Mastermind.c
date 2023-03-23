822e4cbf65668de32e25b7b2a77bccc1a8417902
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-23 09:21:30 -0700

    Create Mastermind.c

---
+++ Mastermind.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+Int MasterMind()
+{
+	PatternGenerator();
+	int guess;
+// create if loop, if the answer is correct, print 1, if incorrect, print 0
+	if (guess == r1)
+	{
+		printf(“1 ”);
+  }
+  if (guess != r1)
+  {
+	  printf(“0 ”);
+  }
+  
+  if (guess == r2)
+	{
+	  printf(“1 ”);
+  }
+  if (guess != r2)
+  {
+	  printf(“0 ”);
+  }
+  
+  if (guess == r3)
+	{
+		printf(“1 ”);
+  }
+  if (guess != r3)
+  {
+	  printf(“0 ”);
+  }
+  
+  if (guess == r4)
+	{
+		printf(“1 ”);
+  }
+  if (guess != r4)
+  {
+	  printf(“0 ”);
+  }
+
+{
