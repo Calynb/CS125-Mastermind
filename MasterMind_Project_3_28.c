66b188da93b9019b233b91169fe2b0f9f0401b0a
mhyndman21 <72372743+mhyndman21@users.noreply.github.com>
2023-03-28 10:16:00 -0700

    Create MasterMind_Project_3_28.c

---
+++ MasterMind_Project_3_28.c
@@ -0,0 +1,25 @@
+#include "PatternGen.h" 
+// Prototypes of functions used
+int MasterMind();
+char numbers_to_colors(int);
+int main()
+{
+   char ans1;
+   char ans2;
+   FILE* f =  fopen("rules.txt" , "r");   
+   char rules[500];
+    
+   printf ("Do you wanna play Mastermind? (y/n) ");
+   scanf("%c" , &ans1);   
+
+   if (ans1 == 'y') {
+      fgets(rules, 500, f);             //prints the rules from the text file
+      printf("%s \n" , rules);
+   }
+   printf ("Are you ready to play? (y/n)");
+   scanf(" %c" , &ans2);
+      while (ans2 == 'y') { 
+      MasterMind();
+      printf("Do you want to play again?\n");  
+      scanf("%c" , &ans2);
+      }
