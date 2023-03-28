6b2316b45410bf9ed32603fe72aa91165a6db924
mhyndman21 <72372743+mhyndman21@users.noreply.github.com>
2023-03-28 10:16:44 -0700

    Create MasterMind_3_28.c

---
+++ MasterMind_3_28.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "PatternGen.h"
+
+
+
+int MasterMind()
+{
+   rand_nums rn;
+   rn=PatternGenerator();
+   char guess1, guess2, guess3, guess4;
+   int count = 0;
+ 
+  printf("Type your 4 guesses: \n");
+  scanf(" %c%*c%c%*c%c%*c%c" , &guess1, &guess2, &guess3, &guess4);
+  printf("GUESS 1=%c GUESS2=%c GUESS3=%c GUESS4=%c \n", guess1, guess2, guess3, guess4);
+     
+  while ((guess1 != rn.num_1) || (guess2 != rn.num_2) || (guess3 != rn.num_3) || (guess4 != rn.num_4) || (count <12)) {
+// create if loop, if the answer is correct, print 1, if incorrect, print 0
+    if (guess1 == rn.num_1){
+	printf("1 ");
+    }
+    if (guess1 != rn.num_1){
+	printf("0 ");
+    }
+    if (guess2 == rn.num_2){
+	printf("1 ");
+    }
+    if (guess2 != rn.num_2){
+	printf("0 ");
+    }
+    if (guess3 == rn.num_3){
+	printf("1 ");
+    }
+    if (guess3 != rn.num_3){
+	printf("0 ");
+    }
+    if (guess4 == rn.num_4){
+	printf("1 ");
+    }
+    if (guess4 != rn.num_4){
+	printf("0 \n");
+    }
+  count = count + 1;
+  printf(" Count: %d \n" ,count);
+	  
+  printf("Type your 4 guesses: \n");
+  scanf(" %c%*c%c%*c%c%*c%c", &guess1, &guess2, &guess3, &guess4);
+  }
+
+}
