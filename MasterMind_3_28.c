70a97690911195616164e94d89278780b40632b9
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-29 15:04:56 -0700

    Update MasterMind_3_28.c

--- MasterMind_3_28.c
+++ MasterMind_3_28.c
@@ -14,7 +14,7 @@ int MasterMind()
   scanf(" %c%*c%c%*c%c%*c%c" , &guess_1, &guess_2, &guess_3, &guess_4);
   printf("GUESS 1=%c GUESS2=%c GUESS3=%c GUESS4=%c \n", guess_1, guess_2, guess_3, guess_4);
      
-  while ((guess_1 != rn.num_1) || (guess_2 != rn.num_2) || (guess_3 != rn.num_3) || (guess_4 != rn.num_4) || (count <12)) {
+  while ((guess_1 != rn.num_1) || (guess_2 != rn.num_2) || (guess_3 != rn.num_3) || (guess_4 != rn.num_4)) {
 // if loops: if the answer is correct, print 1, if incorrect, print 0
     if (guess_1 == rn.num_1){
 	printf("1 ");
@@ -42,9 +42,10 @@ int MasterMind()
     }
   count = count + 1;
   printf(" Count: %d \n" ,count);
-	  // if ((guess_1 != rn.num_1) && (guess_2 != rn.num_2) && (guess_3 != rn.num_3) && (guess_4 != rn.num_4)) {
-	  // break
-          // }
+    if (count > 12){
+	 printf("Sorry, you are out of turns. \n ");
+	 break;
+    }
   printf("Type your 4 guesses: \n");
   scanf(" %c%*c%c%*c%c%*c%c", &guess_1, &guess_2, &guess_3, &guess_4);
   }
