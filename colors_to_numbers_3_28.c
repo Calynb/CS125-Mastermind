e23a46115ab876f7d1538bcc09b7fa07f18be473
Calynb <127427279+Calynb@users.noreply.github.com>
2023-03-30 09:14:09 -0700

    Update colors_to_numbers_3_28.c

--- colors_to_numbers_3_28.c
+++ colors_to_numbers_3_28.c
@@ -1,45 +1,41 @@
-// George's version :)
+/************
+* Calyn, Lauren, and Monica
+* CS 125 Project
+* This file takes the user's guesses and compares them to the code until they get all 4 correct.
+* Works cited: George helped us return the rand_nums rn.
+*************/
 char numbers_to_colors(int number)
 {
   if (number==2)
   {
     return 'R';
-  }
-  
+  }  
   else if (number==3)
   {
     return 'B';
-  }
-  
+  }  
   else if (number==4)
   {
     return 'G';
-  }
-  
+  }  
   else if (number==5)
   {
     return 'Y';
-  }
-  
+  }  
   else if (number==6)
   {
     return 'P';
-  }
-  
+  }  
   else 
   {
     return 'C';
-  }
-  
+  }  
 }
-
 rand_nums PatternGenerator() 
 {
   srand(time(NULL));
-  
   // declare random numbers
     rand_nums rn;
-  
   rn.num_1= numbers_to_colors(rand()%6+2);
   rn.num_2= numbers_to_colors(rand()%6+2);
   rn.num_3= numbers_to_colors(rand()%6+2);
