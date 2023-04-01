6fde383f48e23868b77aef99870b6f48b024d161
Calynb <127427279+Calynb@users.noreply.github.com>
2023-04-01 15:20:15 -0700

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,3 +1,9 @@
+/************
+* Author: Calyn Bauer, Lauren Bailey, and Monica Hyndman
+* CS 125 Project
+* Purpose: This header file contains all the used header files
+* Version 1.0  April 1, 2023
+*************/
 
 #include <stdio.h>
 #include <stdlib.h>
