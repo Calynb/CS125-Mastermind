eb0378c260dd630afe7e7caf28776c5106fb4d45
Calynb <127427279+Calynb@users.noreply.github.com>
2023-04-02 08:26:30 -0700

    Update colors_to_numbers_3_28.c

--- colors_to_numbers_3_28.c
+++ colors_to_numbers_3_28.c
@@ -3,7 +3,7 @@
 * CS 125 Project
 * Purpose: Converts numbers to colors. Colors correspond to a capital letter. 
 * Resources: George helped us return the rand_nums rn.
-* Version 1.0  Mar 7, 2023
+*  Version 1.0  Mar 7, 2023
 *  Version 1.1  Mar 9, 2023
 *  Version 2.0  Mar 21, 2023
 *  Version 2.1  Mar 23, 2023
@@ -15,37 +15,36 @@
 char numbers_to_colors(int number)
 {
   // create if loops to assign a number to a color/letter.
-  if (number==2) // Red=2=R
+  if (number==2)                               // Red=2=R
   {
     return 'R';
   }  
-  else if (number==3) // Blue=3=B
+  else if (number==3)                          // Blue=3=B
   {
     return 'B';
   }  
-  else if (number==4) // Green=4=G
+  else if (number==4)                          // Green=4=G
   {
     return 'G';
   }  
-  else if (number==5) //yellow=5=Y
+  else if (number==5)                          // Yellow=5=Y
   {
     return 'Y';
   }  
-  else if (number==6) //Pink=6=P
+  else if (number==6)                          // Pink=6=P
   {
     return 'P';
   }  
-  else // cyana=C
+  else                                         // Cyana=C
   {
     return 'C';
   }  
 }
 rand_nums PatternGenerator() 
-{
-  // seed generator
-  srand(time(NULL));
-  // declare random numbers
-    rand_nums rn;
+{                               
+  srand(time(NULL));                            // seed generator
+                                 
+    rand_nums rn;                               // declare random numbers
   rn.num_1= numbers_to_colors(rand()%6+2);
   rn.num_2= numbers_to_colors(rand()%6+2);
   rn.num_3= numbers_to_colors(rand()%6+2);
