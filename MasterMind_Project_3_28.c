00082ff494fc3e591c7f6721fc35a47abfe13406
mhyndman21 <72372743+mhyndman21@users.noreply.github.com>
2023-04-01 17:39:24 -0700

    Update MasterMind_Project_3_28.c

--- MasterMind_Project_3_28.c
+++ MasterMind_Project_3_28.c
@@ -12,30 +12,37 @@
 *  Version 1.6  Mar 30, 2023
 *************/
 #include "PatternGen.h" 
-#include "headers.h"
-
-int MasterMind();                            // Prototypes of functions used:
+# include "headers.h"
+// Prototypes of functions used
+int MasterMind();
 char numbers_to_colors(int);
 int main()
 {
-   char ans_1;
-   char ans_2;
+   char ans1;
+   char ans2;
+   int x; // for loop veriable
    FILE* f =  fopen("rules.txt" , "r");   
    char rules[500];
     
    printf ("Do you wanna play Mastermind? (y/n) ");
-   scanf("%c" , &ans_1);   
+   scanf("%c" , &ans1);   
+   printf(" \n");
 
-   if (ans_1 == 'y') {
-      fgets(rules, 500, f);                  // Prints the rules from the text file
-      printf("%s \n" , rules);
-   }
-   printf("Are you ready to play? (y/n)");
-   scanf(" %c" , &ans_2);
-      while (ans_2 == 'y') { 
-      MasterMind();                         // Runs the game function
-      printf("Do you want to play again?\n");  
-      scanf("%c" , &ans_2);
+   if (ans1 == 'y'|| ans1=='Y') {
+      for (x=0;x<1; x++){
+        fgets(rules, 500, f);             //prints the rules from the text file
+        printf("%s" , rules);
+      }
+  
+   printf(" \n");
+   printf ("\nAre you ready to play? (y/n) ");
+   scanf(" %c" , &ans2);
+      while (ans2 == 'y' || ans2=='Y') { 
+      MasterMind();
+      printf("Do you want to play again? (y/n) ");  
+      scanf(" %c" , &ans2);
+      
+      }
       }
    printf("Have a nice day!\n");
 }
