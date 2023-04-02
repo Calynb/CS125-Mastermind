d0cfbdd4d4bc6a7e1b9899ee44197e6f271f2731
Calynb <127427279+Calynb@users.noreply.github.com>
2023-04-02 08:19:02 -0700

    Update MasterMind_Project_3_28.c

--- MasterMind_Project_3_28.c
+++ MasterMind_Project_3_28.c
@@ -3,24 +3,25 @@
 * CS 125 Project
 * Purpose: This file asks the user if they want to play mastermind, 
 * then runs the MasterMind function if they do.
-* Version 1.0  Mar 7, 2023
+*  Version 1.0  Mar 7, 2023
 *  Version 1.1  Mar 9, 2023
 *  Version 1.2  Mar 21, 2023
 *  Version 1.3  Mar 23, 2023
 *  Version 1.4  Mar 28, 2023
 *  Version 1.5  Mar 29, 2023
 *  Version 1.6  Mar 30, 2023
+*  Version 1.7  Apr 1, 2023 
 *************/
 #include "PatternGen.h" 
 # include "headers.h"
-// Prototypes of functions used
-int MasterMind();
+
+int MasterMind();                             // Prototypes of functions used
 char numbers_to_colors(int);
 int main()
 {
    char ans1;
    char ans2;
-   int x; // for loop veriable
+   int x;                                     // for loop variable
    FILE* f =  fopen("rules.txt" , "r");   
    char rules[500];
     
@@ -30,19 +31,17 @@ int main()
 
    if (ans1 == 'y'|| ans1=='Y') {
       for (x=0;x<1; x++){
-        fgets(rules, 500, f);             //prints the rules from the text file
+        fgets(rules, 500, f);                //prints the rules from the text file
         printf("%s" , rules);
       }
-  
    printf(" \n");
    printf ("\nAre you ready to play? (y/n) ");
    scanf(" %c" , &ans2);
       while (ans2 == 'y' || ans2=='Y') { 
-      MasterMind();
+      MasterMind();                          // Calls the mastermind function
       printf("Do you want to play again? (y/n) ");  
       scanf(" %c" , &ans2);
-      
       }
-      }
+   }
    printf("Have a nice day!\n");
 }
